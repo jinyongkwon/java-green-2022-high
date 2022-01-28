7c935e0be939f8cd09020cc9ece19afb7b1d4416
JY <JY@naver.com>
2022-01-28 12:34:10 +0900

    3. thread

--- src/site/metacoding/ex23/ThreadEx02.java
+++ src/site/metacoding/ex23/ThreadEx02.java
@@ -13,29 +13,27 @@ class NewWorker implements Runnable {
                 e.printStackTrace();
             }
         }
-
     }
+}
 
-    public class ThreadEx02 {
+public class ThreadEx02 {
 
-        // MainThread의 타겟은 main 메서드
-        // 타겟 = task : 임무
-        public void main(String[] args) {
+    // MainThread의 타겟은 main 메서드
+    // 타겟 = task : 임무
+    public static void main(String[] args) {
 
-            // 일의 순서가 없는걸 비동기 프로그램이라함.
-            // NewWorker 생성
-            Thread NewWorker = new Thread(new NewWorker());
-            NewWorker.start(); // run() 메서드 호출
-            // 메인과 런이 같이실행됨
+        // 일의 순서가 없는걸 비동기 프로그램이라함.
+        // NewWorker 생성
+        Thread NewWorker = new Thread(new NewWorker());
+        NewWorker.start(); // run() 메서드 호출
+        // 메인과 런이 같이실행됨
 
-            for (int i = 1; i < 6; i++) {
-                try {
-                    System.out.println("메인스레드 : " + i);
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
+        for (int i = 1; i < 6; i++) {
+            try {
+                System.out.println("메인스레드 : " + i);
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
